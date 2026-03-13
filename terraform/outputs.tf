b22d8012f3b1307e10cb1e4743a5c9d719b6a3e8
Sanket Sanjay Hanchate <159873789+CoderSanku@users.noreply.github.com>
2026-03-13 12:59:55 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,11 @@
 output "instance_public_ip" {
-  description = "Public IP of the web server"
+  description = "Public IP of the EC2 instance"
   value       = aws_instance.web.public_ip
+}
+
+output "instance_private_ip" {
+  description = "Private IP of the EC2 instance"
+  value       = aws_instance.web.private_ip
 }
 
 output "app_url" {
@@ -8,3 +13,7 @@ output "app_url" {
   value       = "http://${aws_instance.web.public_ip}:3000"
 }
 
+output "instance_id" {
+  description = "EC2 Instance ID"
+  value       = aws_instance.web.id
+}
